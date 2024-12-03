506b6176b7a7e62b5f719f7c8cdc1e20ee40f036
Sajeet07 <sajitgurung829@gmail.com>
2024-12-03 14:18:02 +0545

    Javascript tutorials with projects

---
+++ index.js
@@ -0,0 +1,80 @@
+//JS variables-It is the container to store data values
+ /*DATA types
+  1.numbers
+var number1 = 34;
+var number2 = 45;
+console.log(number1 + number2);
+ 2.string
+ var str1 = "this is a string";
+ var str2 = "this is also a string";
+  3.boolean
+  var a = true;
+  var b = false;
+  console.log(a,b);
+These are all primitive data types eg;symbol,undefined,null.
+reference data types are array , string etc.
+var marks{
+    sajit = 23,
+    ravi = 55
+}
+console.log(marks);
+            
+           var arr = [1,2,"string",4,5]
+           console.log(arr)
+           TYPES OF ARITHMETIC OPERATORS
+    var a = 100;
+    var b = 10;
+    console.log("the value of a+b is",a+b);
+    console.log("the value of a-b is",a-b);
+    console.log("the value of a/b is",a/b);
+    console.log("the value of a*b is",a*b);
+    assignment operators
+    c = b;
+    c *= 2;
+    console.log(c);
+COMPARISON OPERATOR
+ var a = 55;
+ var b = 66;
+ console.log(a == b);
+ output false
+  console.log(a >= b);
+  output false
+  LOGICAL OPEARTOR
+  console.log(TRUE && TRUE); output true
+  console.log(TRUE || FALSE); outputtrue
+  console.log(!TRUE); it means false
+  FUNCTION
+  function avg(a,b){
+      c = (a+b)/2;
+      return c;
+  }
+  c1 = avg(6,4);
+  c2 = avg(15,15);
+  console.log(c1,c2);
+  output 5,15
+  CONDITIONAL STATMENT
+  var me = 23;
+  if(me>8){
+      console.log('your age is more than 8');
+  }
+  else
+  {console.log('your age is leaa than 8')
+;}
+output your age is more than 8
+ NOW FORM THIS PART OTHER SOURCE CODE IIS IN DESCRIPTION*/
+ /* 0ONLY IMP PART 
+ FOR LOOP 
+ var arr = [1,2,3,5];
+  varr i = 0;
+ forEach(function(element)){   //WE USE forEACH FUNCTION IN REPLACE OF for( varr i=0,i>5,i++)
+     console.log(element)
+ } 
+ output 0,1,2,3,5*/
+ /*ARRAY
+ Let myArr = ["sajit","fan","null"]
+ console.log(myArr.length);
+ const newlen = myArr.unshift["HELLO"];
+ console.log(newLen);
+ console.log(myArr);
+ output 4 // one add vara aaucha unshift garyo vani ani sajit ko thau ma HELLO replace hunxa
+*/
